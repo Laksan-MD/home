292d4ab9b01fa8c20f0afbaf9800ab87844d3334
Laksan-MD <laksanmd@gmail.com>
2023-04-27 21:16:46 +0530

    dvdf

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let increment = document.getElementById("increbtn")
+
+let count = 0
+
+let countEl = document.getElementById("count")
+
+function incre(){
+    count = count + 1
+    countEl.innerText = count
+}
